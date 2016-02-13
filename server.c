9c88e9f8cf6f0c71141a191fa43442cd0ce3783f
Michał Łazowik <mlazowik@gmail.com>
2016-02-13 19:53:28 +0100

    Server: read cli input

--- server.c
+++ server.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
 
-int main() {
-    printf("Hello server!\n");
+int main(int argc, char *argv[]) {
+    assert(argc == 2);
+
+    long int n = strtol(argv[1], NULL, 10);
+
+    assert(n >= 2);
+
+    printf("Hello server! Got %ld\n", n);
 
     return 0;
 }
