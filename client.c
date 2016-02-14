44fbfb7a91ea675bab171ed03be8e30d397aea3c
Michał Łazowik <mlazowik@gmail.com>
2016-02-14 01:49:13 +0100

    Client: read cli input

--- client.c
+++ client.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
 
-int main() {
-    printf("Hello client!\n");
+int main(int argc, char *argv[]) {
+    assert(argc == 2);
+
+    long int op_time = strtol(argv[1], NULL, 10);
+
+    assert(op_time >= 0);
+
+    printf("Hello client! Got %ld\n", op_time);
 
     return 0;
 }
